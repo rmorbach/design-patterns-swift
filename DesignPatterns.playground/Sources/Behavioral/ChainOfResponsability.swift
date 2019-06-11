8ec5064558a61f5b8862cc61e0693d4fa6e816d6
Rodrigo Morbach <morbachrodrigo@gmail.com>
2019-06-10 21:47:16 -0300

    Update chain-of-responsibility

--- DesignPatterns.playground/Sources/Behavioral/ChainOfResponsability.swift
+++ DesignPatterns.playground/Sources/Behavioral/ChainOfResponsability.swift
@@ -27,7 +27,80 @@ class Penny: Coin {
     }
 }
 
+class Dime: Coin {
+    var diameter: Double {
+        return 0.75
+    }
+    var weight: Double {
+        return 2.268
+    }
+    
+    var centValue: Int {
+        return 10
+    }
+}
+
+
+protocol CoinHandlerProtocol {
+    var next: CoinHandlerProtocol? { get }
+    func handleCoinValidation(_ coin: Coin) -> Coin?
+}
+
+class CoinHandler: CoinHandlerProtocol {
+    var next: CoinHandlerProtocol?
+    
+    var coinType: Coin.Type
+    
+    init(withCoinType coinType: Coin.Type) {
+        self.coinType = coinType
+    }
+    
+    func handleCoinValidation(_ coin: Coin) -> Coin? {
+        guard let validatedCoint = self.createCoin(from: coin) else {
+            return next?.handleCoinValidation(coin)
+        }
+        return validatedCoint
+    }
+    
+    private func createCoin(from coin: Coin) -> Coin? {
+        // TODO - perform coin validation
+        return nil
+    }
+}
+
+enum CoinValidationError: Error {
+    case invalidCoin(coin: Coin)
+}
+
+class VendingMachine {
+    let coinHandler: CoinHandler
+    var coins = [Coin]()
+    
+    init(withHandler handler: CoinHandler) {
+        self.coinHandler = handler
+    }
+    
+    func insertCoin(_ coin: Coin) throws {
+        guard let validatedCoin = coinHandler.handleCoinValidation(coin) else {
+            throw CoinValidationError.invalidCoin(coin: coin)
+        }
+        coins.append(validatedCoin)
+    }
+}
+
 public func chainOfResponsabilityExample() {
-    let penny = Penny()
-    print(penny.description)
+    // First handler
+    let pennyHandler = CoinHandler(withCoinType: Penny.self)
+    let dimeHandler = CoinHandler(withCoinType: Dime.self)
+    
+    pennyHandler.next = dimeHandler
+    
+    let vendingMachine = VendingMachine(withHandler: pennyHandler)
+    do {
+        let penny = Penny()
+        try vendingMachine.insertCoin(penny)
+        print(penny.description)
+    } catch {
+        print(error)
+    }
 }
